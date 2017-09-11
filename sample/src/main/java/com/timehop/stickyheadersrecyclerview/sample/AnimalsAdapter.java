787799bd945ddebdb170363bc6943a47273884b5
ichongliang <baichongliang@wallstreetcn.com>
2017-09-11 13:59:02 +0800

    lookup the code

--- sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/AnimalsAdapter.java
+++ sample/src/main/java/com/timehop/stickyheadersrecyclerview/sample/AnimalsAdapter.java
@@ -11,55 +11,55 @@ import java.util.Collection;
  * Adapter holding a list of animal names of type String. Note that each item must be unique.
  */
 public abstract class AnimalsAdapter<VH extends RecyclerView.ViewHolder>
-    extends RecyclerView.Adapter<VH> {
-  private ArrayList<String> items = new ArrayList<String>();
+        extends RecyclerView.Adapter<VH> {
+    private ArrayList<String> items = new ArrayList<String>();
 
-  public AnimalsAdapter() {
-    setHasStableIds(true);
-  }
-
-  public void add(String object) {
-    items.add(object);
-    notifyDataSetChanged();
-  }
-
-  public void add(int index, String object) {
-    items.add(index, object);
-    notifyDataSetChanged();
-  }
-
-  public void addAll(Collection<? extends String> collection) {
-    if (collection != null) {
-      items.addAll(collection);
-      notifyDataSetChanged();
+    public AnimalsAdapter() {
+        setHasStableIds(true);
     }
-  }
 
-  public void addAll(String... items) {
-    addAll(Arrays.asList(items));
-  }
+    public void add(String object) {
+        items.add(object);
+        notifyDataSetChanged();
+    }
 
-  public void clear() {
-    items.clear();
-    notifyDataSetChanged();
-  }
+    public void add(int index, String object) {
+        items.add(index, object);
+        notifyDataSetChanged();
+    }
 
-  public void remove(String object) {
-    items.remove(object);
-    notifyDataSetChanged();
-  }
+    public void addAll(Collection<? extends String> collection) {
+        if (collection != null) {
+            items.addAll(collection);
+            notifyDataSetChanged();
+        }
+    }
 
-  public String getItem(int position) {
-    return items.get(position);
-  }
+    public void addAll(String... items) {
+        addAll(Arrays.asList(items));
+    }
 
-  @Override
-  public long getItemId(int position) {
-    return getItem(position).hashCode();
-  }
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
 
-  @Override
-  public int getItemCount() {
-    return items.size();
-  }
+    public void remove(String object) {
+        items.remove(object);
+        notifyDataSetChanged();
+    }
+
+    public String getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).hashCode();
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
 }
